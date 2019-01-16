b30e486143e6155a05faef19ee1f38acfcbbdbab
Łukasz Kałużny <lukasz@kaluzny.pro>
2019-01-16 22:40:16 +0100

    first version of apps

---
+++ cat/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/tomasen/realip"
+)
+
+func pet(w http.ResponseWriter, r *http.Request) {
+	clientIP := realip.FromRequest(r)
+	log.Println("GET /pet from", clientIP)
+	message := "Cats Are Better Than Dogs!!!"
+	w.Write([]byte(message))
+}
+func main() {
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+	logger.Println("Server is starting...")
+	http.HandleFunc("/", pet)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
