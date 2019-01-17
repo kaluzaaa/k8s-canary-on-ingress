e896799cf0075a4718a68c4660a2cbeaac580003
Łukasz Kałużny <lukasz@kaluzny.pro>
2019-01-17 17:43:24 +0100

    Add Access-Control-Allow-Headers

--- dog/main.go
+++ dog/main.go
@@ -10,6 +10,7 @@ import (
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Headers", "*")
 }
 
 func pet(w http.ResponseWriter, r *http.Request) {
