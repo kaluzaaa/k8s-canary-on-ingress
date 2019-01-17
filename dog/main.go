1e6c1564844e07a3210b4f9d0516d011b65a3479
Łukasz Kałużny <lukasz@kaluzny.pro>
2019-01-17 16:47:36 +0100

    Add CORS

--- dog/main.go
+++ dog/main.go
@@ -8,12 +8,18 @@ import (
 	"github.com/tomasen/realip"
 )
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
 func pet(w http.ResponseWriter, r *http.Request) {
 	clientIP := realip.FromRequest(r)
 	log.Println("GET /pet from", clientIP)
 	message := "Dogs are always happy to see you!"
+	enableCors(&w)
 	w.Write([]byte(message))
 }
+
 func main() {
 	logger := log.New(os.Stdout, "", log.LstdFlags)
 	logger.Println("Server is starting...")
